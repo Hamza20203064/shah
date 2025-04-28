769b3955ff6212c1008805b8d942329e3d0f4926
HAMZA AHMAD <hamzaahmadhashim9@gmail.com>
2025-04-28 11:51:45 +0530

    checking packages

---
+++ test_imports.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import tflite_runtime
+import mediapipe as mp
+
+print("NumPy version:", np.__version__)
+print("OpenCV version:", cv2.__version__)
+print("TFLite Runtime version:", tflite_runtime.__version__)
+print("MediaPipe version:", mp.__version__)
+
+print("\nAll packages imported successfully!")
